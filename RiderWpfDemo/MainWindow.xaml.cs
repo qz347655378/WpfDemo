57be11992a797c38928654047f55f9e8703ee5c7
Eric <347655378@qq.com>
2021-10-16 19:30:44 +0800

    添加项目文件。

---
+++ RiderWpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RiderWpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinWindow_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaxWindow_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MaxWindow.ToolTip = "最大化";
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                MaxWindow.ToolTip = "还原";
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
