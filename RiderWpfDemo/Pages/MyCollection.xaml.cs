1b2b0a2e084600fae5ae7ac2d20f7282ee55ca97
Eric <347655378@qq.com>
2021-10-29 17:19:49 +0800

    自定义文本框

--- RiderWpfDemo/Pages/MyCollection.xaml.cs
+++ RiderWpfDemo/Pages/MyCollection.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RiderWpfDemo.Pages
 {
@@ -24,5 +12,19 @@ namespace RiderWpfDemo.Pages
         {
             InitializeComponent();
         }
+    }
+
+
+    public class CustomTextBox : TextBox
+    {
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(CustomTextBox), new PropertyMetadata(null));
+
     }
 }
